996259a854b464ab84a478da9039552ea8bdc261
MartinLichtblau <martin.lichtblau@gmail.com>
2019-02-21 15:16:10 +0100

    comment all todos

--- ProgrammingSkillsTest.java
+++ ProgrammingSkillsTest.java
@@ -1,7 +1,10 @@
-import java.io.*;
+import java.io.BufferedReader; // import only what's necessary, to make it explicit, performance, for IDE usage.
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Collections;
+import java.util.Collections; // @TODO remove unused imports
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
@@ -26,26 +29,38 @@ import java.util.Map;
  * @author zesch
  * @version 2 #change filename to match class, put version in doc and use version control for keeping track.
  */
-public class ProgrammingSkillsTest {
-    
-    public final static String CHARSET = "ISO-8859-1";
+
+/*
+    @TODO put all overall changes in separate block
+    Definitions:
+        Style Guide = Google Java Style Guide
+
+    @TODO remove unnecessary line breaks based on
+ */
+public class ProgrammingSkillsTest { // @TODO this is a tokenizer, should call it such. But am I allowed to change classname?
+    // @TODO remove line spacing between variables
+    public static final String CHARSET = "ISO-8859-1"; // #style #change order of modifiers
 
     public File inputDir;
     
-    public double nrofFiles;
+    public double nrofFiles; // @TODO remove unused code in general
     
-    public int minimumCharacters;
+    public int minimumCharacters; // @TODO rename to minTokenLength and max ... Reason naming should be consistent and concise.
     public int maximumCharacters;
     
-    HashMap<String, Integer> tokenMap; 
+    HashMap<String, Integer> tokenMap; // @TODO add variable description. What is the string and integer
+    // @TODO the only var that could be public
+    // @TODO use map instead of HashMap for more freedom without a downside.
     
     public ProgrammingSkillsTest(File pInputDir, int pMinChars, int pMaxChars) {
+        // @TODO rename. Don't use prefixes (Hungarian Notation)
+        // Constructor needs to be public?
         
-        inputDir = pInputDir;
+        inputDir = pInputDir; // @TODO this.inputDir = inputDir
         minimumCharacters = pMinChars;
         maximumCharacters = pMaxChars;
         
-        if ((pMinChars == 0) || (pMaxChars == 0)) {
+        if ((pMinChars == 0) || (pMaxChars == 0)) { // @TODO max must be equal or bigger than min. Give explanation in error.
             throw new RuntimeException("Configuration parameters have not been correctly initialized.");
         }
     }
@@ -57,8 +72,9 @@ public class ProgrammingSkillsTest {
     }
     
     private void readFiles() {
-        
-        File[] files = inputDir.listFiles();
+
+        // @TODO consider subfolders
+        File[] files = inputDir.listFiles(); // If Java 8 I would use Files.walk(). With Java 7 Files.newDirectoryStream()
         if (files == null) {
             System.err.println("Filelist is empty. Directory: " + inputDir.getAbsolutePath());
             System.exit(1);
@@ -74,10 +90,11 @@ public class ProgrammingSkillsTest {
             
             System.out.println(file.getAbsolutePath());
             
-            tokenMap = getFileTokens(file);
+            tokenMap = getFileTokens(file); // @TODO merge existing tokens with new tokens. ! same tokens
         }
     }
-    
+
+    // @TODO put tokens directly in tokenMap? Would save much effort.
     private HashMap<String, Integer> getFileTokens(File infile) {
         
         HashMap<String, Integer> fileTokens = new HashMap<String, Integer>();
@@ -87,11 +104,11 @@ public class ProgrammingSkillsTest {
         try {
             in = new BufferedReader(new InputStreamReader(new FileInputStream(infile), CHARSET));
             while ((line = in.readLine()) != null) {
-                String lineParts[] = line.split(" ");
+                String lineParts[] = line.split(" "); // split text in tokens @TODO rename parts into tokens
                 for (String part : lineParts) {
                     if (fileTokens.containsKey(part)) {
-                        fileTokens.put(part, fileTokens.get(part) + 1);
-                    }
+                        fileTokens.put(part, fileTokens.get(part) + 1); // increment occurrence count @TODO use replace to make it specific
+                    } // @TODO check parenthesis formatting
                     else {
                         fileTokens.put(part, 1);
                     }    
@@ -100,6 +117,7 @@ public class ProgrammingSkillsTest {
             in.close();
         } catch (Exception e) {
             System.err.println(e);
+            // @TODO do appropriate error handling, file would stay open, refactor the whole function?
         }
 
         return fileTokens;
@@ -112,11 +130,12 @@ public class ProgrammingSkillsTest {
                         tokenMap.remove(token);
                 }
             }
-        }
-        catch(Exception e) {}
+        } // @TODO remove line break before catch
+        catch(Exception e) {} // @TODO appropriately handle exception
     }
     
     private void outputTokens() {
+        // @TODO directly print foreach or use stringbuilder. Since no fx no return don't use var.
         String output = "";
         for (String token : tokenMap.keySet()) {
             output += token + "\n";
@@ -126,10 +145,11 @@ public class ProgrammingSkillsTest {
     
     public static void main(String[] args) {
         if (args.length != 3) {
-            System.err.println("Wrong number of parameters: java <application> <indir> <minChars> <maxChars>");
+            System.err.println("Wrong number of parameters: java <application> <indir> <minChars> <maxChars>"); // @TODO seems strange....
             System.exit(1);
         }
 
+        // @TODO don't use vars, use directly as parameters
         File inputDir = new File(args[0]);
         int minChars = new Integer(args[1]);
         int maxChars = new Integer(args[2]);
